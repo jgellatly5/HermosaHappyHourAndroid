77b35c7ed4fba8ac2bf01efb9138727db84ed107
Jordan Gellatly <jgellatly5@gmail.com>
2022-11-20 12:00:02 -0800

    Scaling image for detail screen.

--- app/src/main/java/com/jordangellatly/hermosahappyhour/DetailScreen.kt
+++ app/src/main/java/com/jordangellatly/hermosahappyhour/DetailScreen.kt
@@ -1,9 +1,7 @@
 package com.jordangellatly.hermosahappyhour
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -12,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -56,12 +55,11 @@ fun DetailScreen(
                         ?: R.drawable.tower12
                 ),
                 contentDescription = null,
-                modifier = Modifier
-                    .size(130.dp)
-                    .padding(8.dp),
-                contentScale = ContentScale.Fit,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop,
             )
             Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.padding(8.dp)
             ) {
                 Text(
