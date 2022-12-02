ddd760a3958ede48655b0f34fb2596344e7bd85f
Jordan Gellatly <jgellatly5@gmail.com>
2022-12-01 18:08:28 -0800

    Customize style for headers and dividers.

--- app/src/main/java/com/jordangellatly/hermosahappyhour/ui/home/HomeScreen.kt
+++ app/src/main/java/com/jordangellatly/hermosahappyhour/ui/home/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.jordangellatly.hermosahappyhour.ui.home
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -15,8 +17,8 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.jordangellatly.hermosahappyhour.ui.components.BottomNavigationBar
 import com.jordangellatly.hermosahappyhour.RestaurantViewModel
+import com.jordangellatly.hermosahappyhour.ui.components.BottomNavigationBar
 import com.jordangellatly.hermosahappyhour.ui.theme.HermosaHappyHourTheme
 import java.text.SimpleDateFormat
 import java.util.*
@@ -32,91 +34,109 @@ fun HomeScreen(
         },
         backgroundColor = HermosaHappyHourTheme.colors.uiBackground
     ) { contentPadding ->
-        LazyColumn(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(contentPadding),
-            contentPadding = PaddingValues(16.dp)
+                .verticalScroll(rememberScrollState())
+                .padding(contentPadding)
         ) {
-            item {
-                Row(
-                    modifier = Modifier
-                        .padding(top = 40.dp)
-                        .fillMaxWidth()
-                        .wrapContentHeight(),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    val date = getCurrentDateTime()
-                    val dateInString = date.toString("EEEE, MMM d, yyyy")
-                    Text(
-                        text = dateInString,
-                        style = MaterialTheme.typography.h5,
-                        color = HermosaHappyHourTheme.colors.textSecondary
-                    )
-                }
-                Divider(color = HermosaHappyHourTheme.colors.textSecondary)
-            }
-            item {
+            Row(
+                modifier = Modifier
+                    .padding(top = 40.dp, bottom = 8.dp)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val date = getCurrentDateTime()
+                val dateInString = date.toString("EEEE, MMM d, yyyy")
                 Text(
-                    text = "Featured Deals",
-                    style = MaterialTheme.typography.h6,
+                    text = dateInString,
+                    style = MaterialTheme.typography.h5,
                     color = HermosaHappyHourTheme.colors.textSecondary
                 )
             }
-            items(restaurantViewModel.sampleHomeRestaurantData) { restaurant ->
-                RestaurantCard(
-                    name = restaurant.name,
-                    image = restaurant.image
-                ) {
-                    navController.navigate("detail/${restaurant.name}")
+            Divider(
+                color = HermosaHappyHourTheme.colors.uiBorder,
+                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
+            )
+            Text(
+                text = "Featured Deals",
+                style = MaterialTheme.typography.h6,
+                color = HermosaHappyHourTheme.colors.textSecondary,
+                modifier = Modifier.padding(start = 16.dp, top = 8.dp, bottom = 8.dp)
+            )
+            LazyRow {
+                items(restaurantViewModel.sampleSearchRestaurantData) { restaurant ->
+                    RestaurantCard(
+                        name = restaurant.name,
+                        image = restaurant.image
+                    ) {
+                        navController.navigate("detail/${restaurant.name}")
+                    }
                 }
             }
-            item {
-                Text(
-                    text = "Featured Events",
-                    style = MaterialTheme.typography.h6,
-                    color = HermosaHappyHourTheme.colors.textSecondary
-                )
-            }
-            items(restaurantViewModel.sampleHomeRestaurantData) { restaurant ->
-                RestaurantCard(
-                    name = restaurant.name,
-                    image = restaurant.image
-                ) {
-                    navController.navigate("detail/${restaurant.name}")
+            Divider(
+                color = HermosaHappyHourTheme.colors.uiBorder,
+                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
+            )
+            Text(
+                text = "Featured Events",
+                style = MaterialTheme.typography.h6,
+                color = HermosaHappyHourTheme.colors.textSecondary,
+                modifier = Modifier.padding(start = 16.dp, top = 8.dp, bottom = 8.dp)
+            )
+            LazyRow {
+                items(restaurantViewModel.sampleHomeRestaurantData) { restaurant ->
+                    RestaurantCard(
+                        name = restaurant.name,
+                        image = restaurant.image
+                    ) {
+                        navController.navigate("detail/${restaurant.name}")
+                    }
                 }
             }
-            item {
-                Text(
-                    text = "Popular Restaurants",
-                    style = MaterialTheme.typography.h6,
-                    color = HermosaHappyHourTheme.colors.textSecondary
-                )
-            }
-            items(restaurantViewModel.sampleHomeRestaurantData) { restaurant ->
-                RestaurantCard(
-                    name = restaurant.name,
-                    image = restaurant.image
-                ) {
-                    navController.navigate("detail/${restaurant.name}")
+            Divider(
+                color = HermosaHappyHourTheme.colors.uiBorder,
+                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
+            )
+            Text(
+                text = "Popular Restaurants",
+                style = MaterialTheme.typography.h6,
+                color = HermosaHappyHourTheme.colors.textSecondary,
+                modifier = Modifier.padding(start = 16.dp, top = 8.dp, bottom = 8.dp)
+            )
+            LazyRow {
+                items(restaurantViewModel.sampleHomeRestaurantData) { restaurant ->
+                    RestaurantCard(
+                        name = restaurant.name,
+                        image = restaurant.image
+                    ) {
+                        navController.navigate("detail/${restaurant.name}")
+                    }
                 }
             }
-            item {
-                Text(
-                    text = "Newly Added",
-                    style = MaterialTheme.typography.h6,
-                    color = HermosaHappyHourTheme.colors.textSecondary
-                )
-            }
-            items(restaurantViewModel.sampleHomeRestaurantData) { restaurant ->
-                RestaurantCard(
-                    name = restaurant.name,
-                    image = restaurant.image
-                ) {
-                    navController.navigate("detail/${restaurant.name}")
+            Divider(
+                color = HermosaHappyHourTheme.colors.uiBorder,
+                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
+            )
+            Text(
+                text = "Newly Added",
+                style = MaterialTheme.typography.h6,
+                color = HermosaHappyHourTheme.colors.textSecondary,
+                modifier = Modifier.padding(start = 16.dp, top = 8.dp, bottom = 8.dp)
+            )
+            LazyRow {
+                items(restaurantViewModel.sampleHomeRestaurantData) { restaurant ->
+                    RestaurantCard(
+                        name = restaurant.name,
+                        image = restaurant.image
+                    ) {
+                        navController.navigate("detail/${restaurant.name}")
+                    }
                 }
             }
+            Spacer(modifier = Modifier.padding(8.dp))
         }
     }
 }
